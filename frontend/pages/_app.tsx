2145c42066e8d067aa0eb407e24d1fd32a499646
dev-joaovitor <joaotorvitor1@gmail.com>
2025-04-13 12:07:23 -0300

    visual: add `Plus Jakarta Sans` font and apply default font styles

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+    variable: "--font-plus-jakarta-sans",
+    weight: ["500","600","700","800"],
+    subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Component className={plusJakartaSans.className} {...pageProps} />;
 }
